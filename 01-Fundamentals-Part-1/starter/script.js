054d2f6e68f0cdf984e58dfa5ed9650566172221
Mattias <mattiasha23@gmail.com>
2022-01-09 20:36:51 +0100

    fundamentals part 1: values and variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Mattias");
+console.log(23);
+
+let firstName = "Mattias";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// JavaScript SYNTAX CONVENTIONS
+
+// always start variables in lowercase and use camel convention
+let person = 'mattias';
+
+// reserved variable names:
+// - function
+// - new
+// - name (in some cases)
+// you can use _ or $ in front of the reserved name
+let $function = "calculator";
+
+// all upper case is used for variable names that will never change i.e. CONSTANTS
+let PI = 3.1415;
